762c3c2f60c3c5d5d5ff964d8d01ed50133e5fb6
Eonyc <emilson1662@gmail.com>
2022-08-28 17:26:06 -0500

    Se agrega proyecto

---
+++ Gym.App.Dominio/Gym.App.Dominio/Entidades/Credential.cs
@@ -0,0 +1,13 @@
+//Vamos a usar el paquete del sistema
+using System;
+//Definimos la capa a utilizar
+namespace Gym.App.Dominio
+{
+    public class Credential{
+        //Definimos la funcionalidad de Propiedades, usando la
+        //notacion Pascal (1mer caracter del atributo en Mayuscula)
+        public string Email {get; set;}
+        public string Password {get; set;} 
+    } 
+
+}
